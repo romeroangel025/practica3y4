29f5668999a33cb65040414dd47f20e03a105e19
Angel <romeroangel025@gmail.com>
2023-04-29 00:50:57 -0300

    ejercicio 1 y 2

---
+++ funciones.py
@@ -0,0 +1,23 @@
+
+def reverso():
+
+ string = input("Por favor, ingrese su palabra: ")
+
+ string_revertido = string[::-1]
+ 
+ if (string_revertido==string):
+  return print( string, "\n Bingo!")
+ else:
+    return print(string_revertido)
+    
+    
+def tabla_9 ():
+  for numero in range(1, 11):
+      print(".- ""9 *",numero,"=",numero*9,"\n")
+
+
+
+
+
+ 
+  
---
+++ practica_3.py
@@ -0,0 +1,12 @@
+import funciones
+
+"""Escribir un programa, que tome como entrada un string del usuario, el cuál debe ser
+impreso por pantalla en orden reverso. El programa también debe imprimir la palabra
+"Bingo!", si el string ingresado es palíndromo."""
+
+# funciones.reverso()
+
+"""Escribir un algoritmo que muestrre por pantalla las tablas de multiplicacion, desde el 1
+hasta el 9."""
+
+# funciones.tabla_9()
