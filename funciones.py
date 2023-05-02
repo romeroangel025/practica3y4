514206b7b2ebe533c41b344a45aded0ee7343d71
Angel <romeroangel025@gmail.com>
2023-05-02 01:32:11 -0300

    ejercicio 4 y 5

--- funciones.py
+++ funciones.py
@@ -4,20 +4,49 @@ def reverso():
  string = input("Por favor, ingrese su palabra: ")
 
  string_revertido = string[::-1]
- 
- if (string_revertido==string):
-  return print( string, "\n Bingo!")
+
+ if (string_revertido == string):
+  return print(string, "\n Bingo!")
  else:
     return print(string_revertido)
-    
-    
-def tabla_9 ():
+
+
+def tabla_9():
   for numero in range(1, 11):
-      print(".- ""9 *",numero,"=",numero*9,"\n")
+      print(".- ""9 *", numero, "=", numero*9, "\n")
 
 
+def es_palindromo():
+    numero_str = input("ingrese su numero:")
+    # print(type(numero_str))
+    longitud = len(numero_str)
+
+    for i in range(longitud // 2):
+      if numero_str[i] != numero_str[longitud - i - 1]:
+       return print("no es un palindromo")
+
+    return print("es un palindromo")
 
 
-
+def mediaPositivos():
+ suma = 0
+ contador = 0 
+ numero = int(input("Introduzca un número positivo (0 para terminar): "))
+ while numero > 0:
  
-  
+  suma += numero
+  contador += 1
+  numero = int(input("Ingrese un numero positivo (0 para terminar): "))
+
+ if contador == 0:
+    print("Error no se puede calcular la media de 0 numeros ")
+ else:
+    media = int(suma / contador)
+    print("La media de los números ingresados es:", media)
+    
+def sumaPares():
+  suma=0
+  for i in range(2,100,2):
+      suma=i+suma
+  print("La suma es:",suma)
+sumaPares()
--- practica_3.py
+++ practica_3.py
@@ -3,10 +3,26 @@ import funciones
 """Escribir un programa, que tome como entrada un string del usuario, el cuál debe ser
 impreso por pantalla en orden reverso. El programa también debe imprimir la palabra
 "Bingo!", si el string ingresado es palíndromo."""
-
 # funciones.reverso()
 
 """Escribir un algoritmo que muestrre por pantalla las tablas de multiplicacion, desde el 1
 hasta el 9."""
-
 # funciones.tabla_9()
+
+"""Dado un número de 2𝑛 + 1 cifras, chequear si el mismo es palíndromo (capicúa)."""
+# funciones.es_palindromo()
+
+"""Diseñar un algoritmo que calcule la media de una serie de números positivos entrados por teclado.
+ El ingreso de un valor igual a cero indicará el final del ingreso de datos.
+"""
+# mediaPositivos()
+
+"""Diseñar un algoritmo que calcula e imprime la suma de los números pares comprendidos
+entre 2 y 100.
+"""
+# sumaPares()
+"""Diseñar un algoritmo que dado tres números, determinar si la suma de cualquier pareja de
+ellos es igual al tercer número. Si se cumple esta condición deberá imprimir la palabra
+''iguales'', sino ''distintos''.
+"""
+# 
